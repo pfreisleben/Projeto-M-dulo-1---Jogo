5400b098d6dd321142ac5a31a31d1054a54fef33
pfreisleben <pedrofreislebeen@gmail.com>
2021-05-25 20:29:19 -0300

    ok

--- projeto.py
+++ projeto.py
@@ -1,22 +1,24 @@
 # Proposta de projeto de ficção interativa para avaliação de OO
-# Sugestão: completar com classes filhas colocando pessoas saudáveis, trabalhos menos remunerados, casas melhor equipadas et cetera
+# Sugestão: completar com classes filhas colocando pessoas saudáveis,
+# trabalhos menos remunerados, casas melhor equipadas et cetera
 
 class Relogio:
     def __init__(self):
         self.horas = 6
         self.minutos = 0
-    
+
     def __str__(self):
         return f"{self.horas:02d}:{self.minutos:02d}"
-    
+
     def avancaTempo(self, minutos):
         self.minutos += minutos
         while(self.minutos >= 60):
             self.minutos -= 60
             self.horas += 1
-    
+
     def atrasado(self):
         return (self.horas > 7 or (self.horas == 7 and self.minutos > 0))
+
 
 class Personagem:
     def __init__(self):
@@ -25,7 +27,7 @@ class Personagem:
         self.medicado = False
         self.dinheiro = 0
         self.salario = 100
-    
+
     def __str__(self):
         return "Você está " + ("sujo" if self.sujo else "limpo")+", "+("com" if self.fome else "sem")+" fome e "+("" if self.medicado else "não ")+"tomou sua medicação. Você tem "+str(self.dinheiro)+" reais na sua conta."
 
@@ -34,10 +36,12 @@ class Personagem:
         self.fome = True
         self.medicado = False
 
+
 class Casa:
     def __init__(self):
         self.remedios = 1
         self.comida = 5
+
 
 if(__name__ == "__main__"):
     dia = 1
@@ -47,7 +51,8 @@ if(__name__ == "__main__"):
     cafe_da_manha = False
     while True:
         print("---")
-        print("São "+str(relogio)+" do dia "+str(dia)+". Você tem que sair pro trabalho até às 07:00.")
+        print("São "+str(relogio)+" do dia "+str(dia) +
+              ". Você tem que sair pro trabalho até às 07:00.")
         print(personagem)
         print("")
         print("Ações:")
@@ -75,7 +80,8 @@ if(__name__ == "__main__"):
                 personagem.dinheiro -= 15
                 cafe_da_manha = True
             else:
-                print("O café da manhã custa 15 reais, você não tem dinheiro suficiente.")
+                print(
+                    "O café da manhã custa 15 reais, você não tem dinheiro suficiente.")
             relogio.avancaTempo(5)
         elif(opcao == "4"):
             if(cafe_da_manha):
@@ -98,7 +104,8 @@ if(__name__ == "__main__"):
                 personagem.dinheiro -= 20
                 relogio.avancaTempo(10)
             else:
-                print("A cartela com 10 remédios custa 20 reais, você não tem dinheiro suficiente.")
+                print(
+                    "A cartela com 10 remédios custa 20 reais, você não tem dinheiro suficiente.")
                 relogio.avancaTempo(5)
         elif(opcao == "7"):
             print("-=-=-")
@@ -107,24 +114,29 @@ if(__name__ == "__main__"):
             print("-=-=-")
             recebido = personagem.salario
             if(not personagem.medicado):
-                print("Como você não tomou seu remédio, você ficou doente no caminho e não chegou no trabalho")
+                print(
+                    "Como você não tomou seu remédio, você ficou doente no caminho e não chegou no trabalho")
                 recebido = 0
             elif(personagem.sujo):
-                print("Como você estava sujo, seus colegas reclamaram para seu chefe, e você levou uma advertência.")
+                print(
+                    "Como você estava sujo, seus colegas reclamaram para seu chefe, e você levou uma advertência.")
                 recebido *= 0.9
             elif(personagem.fome):
-                print("Como você estava com fome, você trabalhou metade do que consegue trabalhar.")
+                print(
+                    "Como você estava com fome, você trabalhou metade do que consegue trabalhar.")
                 recebido *= 0.5
             elif(relogio.atrasado()):
-                print("Como você chegou atrasado, você produziu menos do que de costume.")
+                print(
+                    "Como você chegou atrasado, você produziu menos do que de costume.")
                 recebido *= 0.8
-            print("Você recebeu "+str(recebido)+" reais pelo seu trabalho hoje.")
+            print("Você recebeu "+str(recebido) +
+                  " reais pelo seu trabalho hoje.")
             print("-=-=-")
 
             personagem.dinheiro += recebido
             personagem.dormir()
             relogio = Relogio()
-            dia+=1
+            dia += 1
         elif(opcao == "0"):
             break
         else:
