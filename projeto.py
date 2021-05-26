74a81a87fa0c4aa34ff4e9cf1438b633d6c3de8c
Italojztrindade <italojztrindade@gmail.com>
2021-05-26 10:21:50 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -3,11 +3,13 @@
 # trabalhos menos remunerados, casas melhor equipadas et cetera
 #   IDEIAS
 # Variar o preço do café da manhã - Italo - Status: Feito
-# desconto de salário em % e talvez até demissão de faltar demais.
+# desconto de salário em % e talvez até demissão se faltar demais.
 # comprar moveis, criar uma lista de produtos - Italo - Status: Em progresso
 # criar a opção de FDS, dia 6 e 7. Talvez um elemento random dentro dos dias, sorteando um feriado - PEDRO
 # randomizar o que for possível.
 # Criação da opção 8, estoque. Italo - Status: Feito mas pode melhorar
+# Criar um segundo menu para as ideias depois do trabalho.
+
 
 import random
 
@@ -35,8 +37,9 @@ class Personagem:
         self.sujo = True
         self.fome = True
         self.medicado = False
-        self.dinheiro = 0
+        self.dinheiro = 10000
         self.salario = 100
+        
 
     def __str__(self):
         return "Você está " + ("sujo" if self.sujo else "limpo")+", "+("com" if self.fome else "sem")+" fome e "+("" if self.medicado else "não ")+"tomou sua medicação. Você tem "+str(self.dinheiro)+" reais na sua conta."
@@ -47,20 +50,47 @@ class Personagem:
         self.medicado = False
 
 
-class Casa:
+class Casa(Personagem):
     def __init__(self):
         self.remedios = 1
         self.comida = 5
+        
 
     def estoque(self):
         return f"Você tem {self.remedios} remedio(s) e {self.comida} alimentos"
 
     def moveis(self):
-        escolha_do_movel = input("Escolha o móvel que deseja: ")
-        estoque_de_moveis = {"Sofá de 3 lugares, retrátil 2.80m":3000.0,
-        "Mesa de jantar com 4 cadeiras":2299.0,
-        
-        }
+        super().__init__()
+        money = self.dinheiro
+        adquiridos = []
+        estoque_de_moveis = {"sofá":3000,
+        "mesa":2299,
+        "video game":1450,
+        "tv":1900}
+        print(estoque_de_moveis)
+        print(type(estoque_de_moveis))
+        escolha_do_movel = input(f"Você tem {money} \nEscolha o móvel que deseja ou sair:").lower()
+        while True:    
+            if escolha_do_movel == "sair":
+                print("Até logo")
+                break
+            else:
+                for item in estoque_de_moveis:
+                    if escolha_do_movel in estoque_de_moveis:
+    
+                        preco = estoque_de_moveis.get(escolha_do_movel)
+                        print(preco)
+                        print(type(preco))
+                        self.dinheiro = money - preco
+                        print(f"{escolha_do_movel} foi adquirido por {preco} e você ficou com {self.dinheiro}")
+                        adquiridos = estoque_de_moveis.popitem()
+                        print(f"Você já tem {adquiridos}")
+                    elif escolha_do_movel in adquiridos or adquiridos == "":
+                        print("Produto indisponivel, você já tem.")
+                        break
+                    else:
+                        print("Produto inválido!")
+                        break
 if(__name__ == "__main__"):
     dia = 1
     relogio = Relogio()
@@ -170,7 +200,8 @@ if(__name__ == "__main__"):
 
         elif(opcao == "8"):
             print(casa.estoque())
-
+        elif(opcao == "9"):
+            casa.moveis()
         elif(opcao == "0"):
             break
         else:
