2e5deed82ffd8b99f6db81efd0c62db35bb57124
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-05-27 14:32:35 -0300

    ok

---
+++ classes.py
@@ -0,0 +1,113 @@
+class Relogio:
+    def __init__(self):
+        self.horas = 6
+        self.minutos = 0
+
+    def __str__(self):
+        return f"{self.horas:02d}:{self.minutos:02d}"
+
+    def avancaTempo(self, minutos):
+        self.minutos += minutos
+        while(self.minutos >= 60):
+            self.minutos -= 60
+            self.horas += 1
+
+    def atrasado(self):
+        return (self.horas > 7 or (self.horas == 7 and self.minutos > 0))
+
+
+class Personagem:
+    def __init__(self):
+        self.sujo = True
+        self.fome = True
+        self.medicado = False
+        self.dinheiro = 10000
+        self.salario = 100
+        self.faltasTrabalho = 7
+        self.desempregado = False
+
+    def __str__(self):
+        return "Você está " + ("sujo" if self.sujo else "limpo")+", "+("com" if self.fome else "sem")+" fome e "+("" if self.medicado else "não ")+"tomou sua medicação. Você tem "+str(self.dinheiro)+" reais na sua conta."
+
+    def dormir(self):
+        self.sujo = True
+        self.fome = True
+        self.medicado = False
+
+
+class Casa():
+    def __init__(self):
+        self.remedios = 1
+        self.comida = 5
+        self.ja_tem = ["cama", "geladeira", "fogão"]
+        self.estoque_de_moveis = {"sofa": 3000,
+                                  "mesa": 2299,
+                                  "video game": 1450,
+                                  "tv": 11900}
+
+    def estoque(self):
+        return f"Você tem {self.remedios} remedio(s) e {self.comida} alimentos"
+
+    def moveis(self, personagem):
+        # super().__init__()
+        print(f'Você já tem: {self.ja_tem}')
+        print(f'Móveis disponiveis para compra: ')
+        print(self.estoque_de_moveis)
+
+        escolha_do_movel = input(
+            f"Você tem {personagem.dinheiro} \nEscolha o móvel que deseja ou sair: ").lower()
+        while True:
+            if escolha_do_movel == "sair":
+                print("Até logo")
+                break
+            else:
+                if escolha_do_movel in self.estoque_de_moveis:
+
+                    preco = self.estoque_de_moveis.get(escolha_do_movel)
+                    if preco <= personagem.dinheiro:
+                        personagem.dinheiro -= preco
+                        self.ja_tem.append(escolha_do_movel)
+                        self.estoque_de_moveis.pop(escolha_do_movel)
+                        return print(f"{escolha_do_movel} foi adquirido por {preco} e você ficou com {personagem.dinheiro}")
+
+                    else:
+                        return print(f"Você não tem dinheiro para comprar esse produto, falta {preco - personagem.dinheiro}")
+
+                else:
+                    return print("Produto inválido!")
+
+
+class Dia:
+    def __init__(self):
+        self.dia = 1
+        self.finalDeSemana = False
+
+    def __str__(self):
+        return f"{self.dia}, {self.semana()}"
+
+    # Se for sáb ou dom, FDS = True.
+    def avancaDia(self):
+        if 6 <= self.dia <= 7:
+            self.finalDeSemana = True
+        else:
+            self.finalDeSemana = False
+        if self.dia == 7:
+            self.dia = 1
+        else:
+            self.dia += 1
+
+    def semana(self):
+        if self.dia == 1:
+            return "Segunda-feira"
+        elif self.dia == 2:
+            return "Terça-feira"
+        elif self.dia == 3:
+            return "Quarta-feira"
+        elif self.dia == 4:
+            return "Quinta-feira"
+        elif self.dia == 5:
+            return "Sexta-feira"
+        elif self.dia == 6:
+            return "Sábado"
+        elif self.dia == 7:
+            return "Domingo"
--- projeto.py
+++ projeto.py
@@ -17,117 +17,7 @@
 import random
 
 
-class Relogio:
-    def __init__(self):
-        self.horas = 6
-        self.minutos = 0
-
-    def __str__(self):
-        return f"{self.horas:02d}:{self.minutos:02d}"
-
-    def avancaTempo(self, minutos):
-        self.minutos += minutos
-        while(self.minutos >= 60):
-            self.minutos -= 60
-            self.horas += 1
-
-    def atrasado(self):
-        return (self.horas > 7 or (self.horas == 7 and self.minutos > 0))
-
-
-class Personagem:
-    def __init__(self):
-        self.sujo = True
-        self.fome = True
-        self.medicado = False
-        self.dinheiro = 10000
-        self.salario = 100
-
-    def __str__(self):
-        return "Você está " + ("sujo" if self.sujo else "limpo")+", "+("com" if self.fome else "sem")+" fome e "+("" if self.medicado else "não ")+"tomou sua medicação. Você tem "+str(self.dinheiro)+" reais na sua conta."
-
-    def dormir(self):
-        self.sujo = True
-        self.fome = True
-        self.medicado = False
-
-
-class Casa():
-    def __init__(self):
-        self.remedios = 1
-        self.comida = 5
-        self.ja_tem = ["cama", "geladeira", "fogão"]
-        self.estoque_de_moveis = {"sofa": 3000,
-                                  "mesa": 2299,
-                                  "video game": 1450,
-                                  "tv": 11900}
-
-    def estoque(self):
-        return f"Você tem {self.remedios} remedio(s) e {self.comida} alimentos"
-
-    def moveis(self, personagem):
-        # super().__init__()
-        print(f'Você já tem: {self.ja_tem}')
-        print(f'Móveis disponiveis para compra: ')
-        print(self.estoque_de_moveis)
-
-        escolha_do_movel = input(
-            f"Você tem {personagem.dinheiro} \nEscolha o móvel que deseja ou sair: ").lower()
-        while True:
-            if escolha_do_movel == "sair":
-                print("Até logo")
-                break
-            else:
-                if escolha_do_movel in self.estoque_de_moveis:
-
-                    preco = self.estoque_de_moveis.get(escolha_do_movel)
-                    if preco <= personagem.dinheiro:
-                        personagem.dinheiro -= preco
-                        self.ja_tem.append(escolha_do_movel)
-                        self.estoque_de_moveis.pop(escolha_do_movel)
-                        return print(f"{escolha_do_movel} foi adquirido por {preco} e você ficou com {personagem.dinheiro}")
-
-                    else:
-                        return print(f"Você não tem dinheiro para comprar esse produto, falta {preco - personagem.dinheiro}")
-
-                else:
-                    return print("Produto inválido!")
-
-
-class Dia:
-    def __init__(self):
-        self.dia = 1
-        self.finalDeSemana = False
-
-    def __str__(self):
-        return f"{self.dia}, {self.semana()}"
-
-    # Se for sáb ou dom, FDS = True.
-    def avancaDia(self):
-        if 6 <= self.dia <= 7:
-            self.finalDeSemana = True
-        else:
-            self.finalDeSemana = False
-        if self.dia == 7:
-            self.dia = 1
-        else:
-            self.dia += 1
-
-    def semana(self):
-        if self.dia == 1:
-            return "Segunda-feira"
-        elif self.dia == 2:
-            return "Terça-feira"
-        elif self.dia == 3:
-            return "Quarta-feira"
-        elif self.dia == 4:
-            return "Quinta-feira"
-        elif self.dia == 5:
-            return "Sexta-feira"
-        elif self.dia == 6:
-            return "Sábado"
-        elif self.dia == 7:
-            return "Domingo"
+from classes import Personagem, Dia, Casa, Relogio
 
 
 if(__name__ == "__main__"):
@@ -139,7 +29,7 @@ if(__name__ == "__main__"):
     while True:
         print("---")
         print("São "+str(relogio)+" do dia "+str(dia) +
-              ". Você tem que sair pro trabalho até às 07:00.")
+              (". Você está desempregado" if personagem.desempregado else ". Você precisa sair pra trabalhar até as 07h00"))
         print(personagem)
         print("")
         print("Ações:")
@@ -207,7 +97,13 @@ if(__name__ == "__main__"):
             print(personagem)
             print("-=-=-")
             recebido = personagem.salario
-            if(not personagem.medicado):
+            if personagem.faltasTrabalho > 5:
+                print(
+                    "Como você faltou ao trabalho mais de 5 vezes, o seu chefe demitiu você. Por justa causa... ")
+                print(f'Você retornou a sua casa. ')
+                relogio.avancaTempo(120)
+                continue
+            elif(not personagem.medicado):
                 print(
                     "Como você não tomou seu remédio, você ficou doente no caminho e não chegou no trabalho")
                 recebido = 0
@@ -240,6 +136,7 @@ if(__name__ == "__main__"):
 
         elif(opcao == "10"):
             personagem.dormir()
+            personagem.faltasTrabalho += 1
             relogio = Relogio()
             dia.avancaDia()
         elif(opcao == "0"):
