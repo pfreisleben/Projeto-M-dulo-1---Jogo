e78e166738b1ffd437d7552c0aebc9078c7521f0
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-05-27 15:34:02 -0300

    ok

--- classes.py
+++ classes.py
@@ -70,6 +70,7 @@ class Casa():
                         return print(f"Você não tem dinheiro para comprar esse produto, falta {preco - personagem.dinheiro}")
                 else:
                     return print("Produto inválido!")
+                    continue
 
 
 class Dia:
