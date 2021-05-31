6a77685d1514115d4dc0a6797ee102b1d9c8e202
pfreisleben <pedrofreislebeen@gmail.com>
2021-05-31 19:33:00 -0300

    ok

--- projeto.py
+++ projeto.py
@@ -1,21 +1,24 @@
 # Ideias implantadas:
 # Opção 8 - Chequar estoque de comida e remédio
 # Opção 9 - Compra de móveis, validando estoque disponivel, dinheiro, e caso compre tudo, ganha o jogo.
-# Variar o preço do café da manhã - Italo - Status: Feito
+# Variar o preço do café da manhã
 # Criação da classe "Dia", responsavél por armazenar a data, e valida se é final de semana ou não.
 # Caso vá dormir sem trabalhar, adiciona uma falta ao trabalho(caso não seja final de semana)
 # Caso seja final de semana, não pode trabalhar, e a opção desaparece do menu.
 # Caso falte mais de 5 vezes no trabalho, é demitido e perde o Jogo.
 # Informe o nome do dia da semana: Segunda, Terça....
 # Opção 10 - O personagem pode dormir direto, avançando um dia.
+# Criado menu adaptado para o final de semana, com opções únicas.
 
-# As ideias implantadas usaram os conceitos de Módulo, Classe, Variavel, Loops, Metodos, Operadores Lógicos e
+# As ideias implantadas usaram os conceitos de Encapsulamento, Módulo, Classe, Variavel, Loops, Metodos, Operadores Lógicos e
 # condicionais.
 
-# A idéia principal foi melhorar e implantar novas funcionalides no projeto, ao invés de criar uma história totalmente
+# A idéia principal foi melhorar e implantar novas funcionalidades no projeto, ao invés de criar uma história totalmente
 # diferente, caso o personagem consiga comprar todos os móveis, ele ganha. Se for demitido, perde.
 
 # Integrantes: Pedro Freisleben, Italo, Marcio Salu Pereira
+# Criamos um projeto no git e todos os integrantes do projeto foram adicionados como colaboradores, então todas as
+# alterações foram feitas através de commits, sincronizando todo o trabalho. Definimos
 
 
 import random
